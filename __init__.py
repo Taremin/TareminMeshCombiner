0e358add4e1516f5d9cf087db30f2f8817b6484c
Taremin <taremin@example.com>
2018-04-30 18:56:58 +0900

    initial commit

---
+++ __init__.py
@@ -0,0 +1,171 @@
+import os
+import sys
+import copy
+import importlib
+import bpy.utils.previews
+
+from datetime import datetime
+
+bl_info = {
+    'name': 'Taremin Blender Plugin',
+    'category': '3D View',
+    'author': 'Taremin',
+    'location': 'View 3D > Tool Shelf > Taremin',
+    'description': "Taremin's private plugin",
+    'version': [0, 0, 1],
+    'blender': (2, 79, 0),
+    'wiki_url': '',
+    'tracker_url': '',
+    'warning': '',
+}
+
+version = copy.deepcopy(bl_info.get('version'))
+
+bpy.types.Scene.remove_unnecessary_objects = bpy.props.BoolProperty(
+    name="余計なオブジェクトの削除",
+    default=True,
+    description="メッシュ、アーマチュア以外のオブジェクトと非表示状態になってるオブジェクトの削除を行う。"
+)
+bpy.types.Scene.rename_uvmaps = bpy.props.BoolProperty(
+    name="UVMapのリネーム",
+    default=True,
+    description="ユーザ設定の翻訳で「新しいデータ」にチェックが入っている場合、英語(UVMap)と日本語(UVマップ)のUVマップがそれぞれ存在する場合がある。\n"
+        "メッシュオブジェクトの結合を行った時、このUVMapが混在していると日本語か英語のUVマップどちらか片方が反映できなくなる場合がある。\n"
+        "UVMapのリネームをオンにしているとオブジェクトの結合を行う前にすべてのUVMapを 'UVMap' にリネームする。"
+)
+
+class OptimizeButton(bpy.types.Operator):
+    bl_idname = 'taremin.optimize'
+    bl_label = '最適化'
+    bl_description = 'FBXエクスポートのためにオブジェクトの整理を行う。'
+    bl_options = {'REGISTER', 'UNDO'}
+
+    def select(self, obj):
+        bpy.context.scene.objects.active = obj
+        obj.select = True
+
+    def execute(self, context):
+        scene = context.scene
+        prev = None
+        meshes = []
+        deletes = []
+        active = bpy.context.scene.objects.active
+
+        if not active:
+            return
+
+        bpy.ops.object.select_all(action='DESELECT')
+        for obj in scene.objects:
+            # アーマチュアはスキップ
+            if (obj.type in ('ARMATURE')):
+                print("{} - Skip".format(obj.name))
+                continue
+            
+            if (prev):
+                prev.select = False
+            self.select(obj)
+            prev = obj
+
+            # メッシュとアーマチュア以外か不可視状態のオブジェクトは削除
+            if (obj.type != 'MESH' or obj.hide):
+                print("{} - Skip (Remove)".format(obj.name))
+                deletes.append(obj)
+                continue
+
+            meshes.append(obj)
+            print("{} - {} ({})".format(obj.name, obj.type, obj.hide))
+            for mod in obj.modifiers:
+                if (mod.type not in ('ARMATURE')):
+                    print("\t{} - Apply".format(mod.type))
+                    bpy.ops.object.modifier_apply(modifier=mod.name)
+                else:
+                    print("\t{} - Skip".format(mod.type))
+
+        #
+        # UVマップのリネーム
+        #
+        if context.scene.rename_uvmaps:
+            print("Rename UVMaps")
+            for obj in scene.objects:
+                if (obj.type not in ('MESH')):
+                    continue
+                for uvmap in obj.data.uv_layers:
+                    print("\tRename {} - {} -> {}".format(obj.name, uvmap.name, 'UVMap'))
+                    uvmap.name = 'UVMap'
+
+        #
+        # メッシュオブジェクトの結合
+        #
+        print("Join all mesh objects")
+        bpy.ops.object.select_all(action='DESELECT')
+        for obj in meshes:
+            print("\t{} - Join".format(obj.name))
+            self.select(obj)
+        if active in meshes:
+            bpy.context.scene.objects.active = active
+        bpy.ops.object.join()
+
+        #
+        # 不可視状態になってる or メッシュ以外のオブジェクトの削除
+        # オブジェクトは不可視状態のままだと削除できてないので解除する必要がある
+        # (Pythonコンソールだと不可視のまま削除できる)
+        #
+        if context.scene.remove_unnecessary_objects:
+            print("Delete all hide objects")
+            bpy.ops.object.select_all(action='DESELECT')
+            for obj in deletes:
+                print("\t{} - Delete".format(obj.name))
+                obj.hide = False
+                self.select(obj)
+                bpy.ops.object.delete()
+
+        # active
+        if active in meshes:
+            bpy.ops.object.select_all(action='DESELECT')
+            self.select(active)
+            bpy.context.scene.objects.active = active
+
+        return {'FINISHED'}
+
+class TareminPanel(bpy.types.Panel):
+    bl_label = 'Taremin Blender Plugin'
+    bl_idname = 'VIEW3D_PT_taremin_v1'
+    bl_label = 'Taremin'
+    bl_region_type = 'TOOLS'
+    bl_space_type = 'VIEW_3D'
+    bl_category = 'Taremin'
+
+    def draw(self, context):
+        layout = self.layout
+        box = layout.box()
+        col = box.column(align=True)
+
+        row = col.row(align=True)
+        row.label('最適化')
+        row = col.row(align=True)
+        row.prop(context.scene, 'remove_unnecessary_objects')
+        row = col.row(align=True)
+        row.prop(context.scene, 'rename_uvmaps')
+        row = col.row(align=True)
+        row.label('結合先のオブジェクトをアクティブにしてから最適化を行ってください。')
+        row = col.row(align=True)
+        row.operator('taremin.optimize')
+
+
+classesToRegister = [
+    TareminPanel,
+    OptimizeButton,
+]
+
+
+def register():
+    for value in classesToRegister:
+        bpy.utils.register_class(value)
+
+
+def unregister():
+    for value in classesToRegister:
+        bpy.utils.unregister_class(value)
+
+if __name__ == '__main__':
+    register()
